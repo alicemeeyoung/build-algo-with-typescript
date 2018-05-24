842a72f10cfadf3071275c878bc645d7fc8d51dd
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-05-23 23:45:37 -0700

    merge sort in progress

---
+++ mergeSort.ts
@@ -0,0 +1,26 @@
+export const mergeSort = (array: number[]): number[] => {
+	if (array.length <= 1) {
+		return array;
+	}
+	const middle = Math.floor(array.length / 2);
+	const left = array.slice(0, middle);
+	const right = array.slice(middle);
+
+	return merge(mergeSort(left), mergeSort(right));
+};
+
+/** Merge (conquer) step of mergeSort */
+const merge = (left: number[], right: number[]): number[] => {
+	const array: number[] = [];
+	let lIndex = 0;
+	let rIndex = 0;
+
+	while (lIndex + rIndex < left.length + right.length) {
+		const lItem = left[lIndex];
+		const rItem = right[rIndex];
+		if (lItem === null) {
+			array.push(rItem);
+			rIndex++;
+		}
+	}
+};
