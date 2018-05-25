62d50262630295e9c1cf363d89d88856ca134d0c
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-05-24 22:58:51 -0700

    complete merge sort

--- mergeSort.ts
+++ mergeSort.ts
@@ -1,26 +1,35 @@
-export const mergeSort = (array: number[]): number[] => {
-	if (array.length <= 1) {
-		return array;
-	}
-	const middle = Math.floor(array.length / 2);
-	const left = array.slice(0, middle);
-	const right = array.slice(middle);
+/**
+ *
+ * time: T(n) = 2 * T(n/2) + O(n)
+ * O(nlogn)
+ */
 
-	return merge(mergeSort(left), mergeSort(right));
+export const mergeSort = (array: number[]): number[] => {
+    console.log(array);
+    if (array.length <= 1) {
+        return array;
+    }
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
 };
 
 /** Merge (conquer) step of mergeSort */
 const merge = (left: number[], right: number[]): number[] => {
-	const array: number[] = [];
-	let lIndex = 0;
-	let rIndex = 0;
+    const array: number[] = [];
+    let lIndex = 0;
+    let rIndex = 0;
 
-	while (lIndex + rIndex < left.length + right.length) {
-		const lItem = left[lIndex];
-		const rItem = right[rIndex];
-		if (lItem === null) {
-			array.push(rItem);
-			rIndex++;
-		}
-	}
+    while (lIndex + rIndex < left.length + right.length) {
+        const lItem = left[lIndex];
+        const rItem = right[rIndex];
+        if (lItem === null) {
+            array.push(rItem);
+            rIndex++;
+        }
+    }
+    console.log({ left, right, array });
+    return array;
 };
